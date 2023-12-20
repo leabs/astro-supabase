a49a9e80e3133c57bf1cfdb46b51b3c69ccf053f
Leabs <stevenleabo@gmail.com>
2023-12-20 16:46:30 -0500

    adding sentry but not importing yet

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
+import sentry from "@sentry/astro";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-supabase-auth.vercel.app",
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs(), sentry()]
 });
